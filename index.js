0f9b69a55c65ee1e0cc876a64015b2d21f140575
Sam Jarvis <jarvisam@gmail.com>
2019-11-01 10:52:35 +0000

    ping

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const listener = require('listener');
-const { findHorizontalThree, findVerticalThree, findDecendingDiagonal, findAscendingDiagonal } = require('detector');
-const { receiveMove, sendMove } = require('brain');
+const listener = require('./listener');
+//const { findHorizontalThree, findVerticalThree, findDecendingDiagonal, findAscendingDiagonal } = require('detector');
+//const { receiveMove, sendMove } = require('brain');
 
 const start = () => {
     const server = listener.listen()
-
-    
+    server.on('connection',(socket)=>{
+        socket.on('data',(data)=>{
+            const message = data.toString()
+            console.log(data.toString())
+            if(!message.includes('HELLO=0') || !message.includes('END') || message.includes('VALID')){
+                console.log('sending &MOVE=1;')
+                socket.write('&MOVE=1;')
+            };
+        })
+        socket.on('end',()=>{
+            console.log('disconnected')
+        })
+    })
 }
+
+start()
