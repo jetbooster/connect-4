f792f6baebda18c51fa530e28f926bd34fa1e96d
Dave <bridge.dave@gmail.com>
2019-11-01 10:48:56 +0000

    Init board

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const listener = require('listener');
 const { findHorizontalThree, findVerticalThree, findDecendingDiagonal, findAscendingDiagonal } = require('detector');
 const { receiveMove, sendMove } = require('brain');
 
+let board;
+
 const start = () => {
     const server = listener.listen()
 
-    
+    board = initialiseBoard();
+}
+
+function initialiseBoard() {
+    return new Array(6).fill(new Array(7).fill(0));
 }
