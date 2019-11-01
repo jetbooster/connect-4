c12f8438469a68bba690a39ee04010287e04a828
Sam Jarvis <jarvisam@gmail.com>
2019-11-01 14:14:45 +0000

    .

--- detector.js
+++ detector.js
@@ -38,50 +38,46 @@ const detectHorizontal = (board) => {
 
 const detectVertical = (board) => {
     // for each row, check for 3 of the same number in a row, followed by an empty space
-    let move;
-    let result;
+    let bestResult;
     board.forEach((column, index)=>{
-        result = check(column,index, move)
+        const result = check(column,index)
+        if (bestResult && !bestResult.colour === 'us'){
+            if (result.colour === 'us'){
+                bestResult = result
+            }
+        } else if(result) {
+            bestResult = result;
+        }
     });
-    if (result){
-        return result.column;
+    if (bestResult){
+        return bestResult.column;
     }
     return -1
 }
 
-const check = (col,index, move) =>{
+const check = (col,index) =>{
+    let move;
     let num = 0;
     let colour = undefined;
     let prevSpace = undefined;
-    console.log(col)
     col.forEach((space)=>{
 
         if(num === 3 && space === 0){
             console.log('three in a row detected')
             // chain of 3 detected and a blank space
             colour = prevSpace === 1 ? 'us' : 'them'
-            if(move && move.colour === 'them' && colour === 'us' ){
-                // this move is better as it ends the game for us
-                // rather than just blocking them
-                move = { player: colour, column:index }
-            } else {
-                move = { player: colour, column:index }
-            }
+            move = { player: colour, column:index }
             return;
         }
         if (space === 0){
-            console.log('Empty space')
             // have reached a 0 without reaching 3 in a row. Stop.
             return;
         }
         if (prevSpace === undefined || space === prevSpace){
             // prev was blank, or the same as previous. add 1 to num
             num +=1;
-            console.log(`chain started ${num}`)
         } else {
             // colours did not match, this is a new chain
-            console.log('colour inverted')
-            console.log({prevSpace,space})
             num = 1;
         }
         prevSpace = space;
